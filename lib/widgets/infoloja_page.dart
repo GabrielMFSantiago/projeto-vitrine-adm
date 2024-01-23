f120c6802966e5307e753615c97fdce3f3c2249a
Danillo98 <58270838+Danillo98@users.noreply.github.com>
2024-01-23 20:48:24 -0300

    Edição Info Loja
    adicionado edição de Informações da Loja

--- lib/widgets/infoloja_page.dart
+++ lib/widgets/infoloja_page.dart
@@ -19,60 +19,133 @@ class _InfoLojaPageState extends State<InfoLojaPage> {
   late String _nomeProprietario;
   late String _telefone;
 
-@override
-void initState() {
-  super.initState();
-  _userId = widget.userId ?? '';
-  print('User ID in initState: $_userId');  
-  _nomeLoja = ''; 
-  _cnpj = '';
-  _endereco = '';
-  _nomeProprietario = '';
-  _telefone = '';
+  @override
+  void initState() {
+    super.initState();
+    _userId = widget.userId ?? '';
+    print('User ID in initState: $_userId');
+    _nomeLoja = '';
+    _cnpj = '';
+    _endereco = '';
+    _nomeProprietario = '';
+    _telefone = '';
 
-  _loadLojaData(); 
-}
-
-
-Future<void> _loadLojaData() async {
-  try {
-    if (_userId.isNotEmpty) {
-      DocumentSnapshot lojaDoc = await FirebaseFirestore.instance
-          .collection('usersadm')
-          .doc(_userId)
-          .get();
-
-      if (lojaDoc.exists) {
-        setState(() {
-          _nomeLoja = lojaDoc['nome'] ?? '';
-          _cnpj = lojaDoc['cnpj'] ?? '';
-          _endereco = lojaDoc['endereco'] ?? '';
-          _nomeProprietario = lojaDoc['nomeProprietario'] ?? '';
-          _telefone = lojaDoc['telefone'] ?? '';
-        });
-      }
-    }
-  } catch (e) {
-    print('Erro ao carregar dados da loja: $e');
-    setState(() {
-      _nomeLoja = ''; 
-      _cnpj = '';
-      _endereco = '';
-      _nomeProprietario = '';
-      _telefone = '';
-    });
+    _loadLojaData();
   }
-}
 
+  Future<void> _loadLojaData() async {
+    try {
+      if (_userId.isNotEmpty) {
+        DocumentSnapshot lojaDoc = await FirebaseFirestore.instance
+            .collection('usersadm')
+            .doc(_userId)
+            .get();
+
+        if (lojaDoc.exists) {
+          setState(() {
+            _nomeLoja = lojaDoc['nome'] ?? '';
+            _cnpj = lojaDoc['cnpj'] ?? '';
+            _endereco = lojaDoc['endereco'] ?? '';
+            _nomeProprietario = lojaDoc['nomeProprietario'] ?? '';
+            _telefone = lojaDoc['telefone'] ?? '';
+          });
+        }
+      }
+    } catch (e) {
+      print('Erro ao carregar dados da loja: $e');
+      setState(() {
+        _nomeLoja = '';
+        _cnpj = '';
+        _endereco = '';
+        _nomeProprietario = '';
+        _telefone = '';
+      });
+    }
+  }
+
+  _editarInformacoes() async {
+    TextEditingController enderecoController = TextEditingController(text: _endereco);
+    TextEditingController telefoneController = TextEditingController(text: _telefone);
+
+    return showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Editar Informações'),
+          contentPadding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              TextField(
+                controller: enderecoController,
+                decoration: InputDecoration(labelText: 'Novo Endereço'),
+              ),
+              SizedBox(height: 8.0),
+              TextField(
+                controller: telefoneController,
+                decoration: InputDecoration(labelText: 'Novo Telefone'),
+                keyboardType: TextInputType.phone,
+                inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+              ),
+            ],
+          ),
+          actions: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: Text('Cancelar'),
+            ),
+            ElevatedButton(
+              onPressed: () async {
+                if (telefoneController.text.length == 11) {
+                  await FirebaseFirestore.instance
+                      .collection('usersadm')
+                      .doc(_userId)
+                      .update({
+                    'endereco': enderecoController.text,
+                    'telefone': telefoneController.text,
+                  });
+
+                  await _loadLojaData();
+
+                  Navigator.pop(context);
+                } else {
+                  showDialog(
+                    context: context,
+                    builder: (BuildContext context) {
+                      return AlertDialog(
+                        title: Text('Erro'),
+                        content: Text('O telefone deve ter 11 dígitos.'),
+                        actions: [
+                          ElevatedButton(
+                            onPressed: () {
+                              Navigator.pop(context);
+                            },
+                            child: Text('OK'),
+                          ),
+                        ],
+                      );
+                    },
+                  );
+                }
+              },
+              child: Text('Alterar'),
+            ),
+          ],
+        );
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     if (_userId.isEmpty) {
       return Scaffold(
         appBar: AppBar(
-          title: const Text('Informações da Loja',style: TextStyle(color: Colors.white),),
-           backgroundColor: Colors.black,
-           iconTheme: IconThemeData(color: Colors.white),
+          title: const Text('Informações da Loja', style: TextStyle(color: Colors.white)),
+          backgroundColor: Colors.black,
+          iconTheme: IconThemeData(color: Colors.white),
         ),
         body: const Center(
           child: Text('O ID do usuário está vazio.'),
@@ -82,22 +155,30 @@ Future<void> _loadLojaData() async {
 
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Informações da Loja',style: TextStyle(color: Colors.white),),
-         backgroundColor: Colors.black,
-         iconTheme: IconThemeData(color: Colors.white),
+        title: const Text('Informações da Loja', style: TextStyle(color: Colors.white)),
+        backgroundColor: Colors.black,
+        iconTheme: IconThemeData(color: Colors.white),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.edit),
+            onPressed: _editarInformacoes,
+          ),
+        ],
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            _buildInfo('Nome da Loja:', _nomeLoja),
-            _buildInfo('CNPJ:', _cnpj),
-            _buildInfo('Endereço:', _endereco),
-            _buildInfo('Nome do Proprietário:', _nomeProprietario),
-            _buildInfo('Telefone:', _telefone),
-            const SizedBox(height: 16.0),
-          ],
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              _buildInfo('Nome da Loja:', _nomeLoja),
+              _buildInfo('CNPJ:', _cnpj),
+              _buildInfo('Endereço:', _endereco),
+              _buildInfo('Nome do Proprietário:', _nomeProprietario),
+              _buildInfo('Telefone:', _telefone),
+              const SizedBox(height: 16.0),
+            ],
+          ),
         ),
       ),
     );
